c0a786c612e1122a73810f8b66c9826cefee52a0
YuraProgStan <yurstan@gmail.com>
2022-01-17 18:17:40 +0200

    bonus_useForm_joi

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 const Form = () => {
+    const {
+register, handleSubmit, watch, formState:{errors}
+
+    } = useForm();
+    const submit = (data) => {
+        carSevice.create(car).then
+    }
+    // watch(event => console.log(event));
     return (
         <div>
-<form>
-    <div><label><input type='text' /></label></div>
-    <div><label><input type='text' /></label></div>
-    <div><label><input type='text' /></label></div>
+<form onSubmit = {handleSubmit(submit)}>
+    <div><label>Model: <input type='text' defaultValue={''} {...register ('model')} /></label></div>
+    <div><label>Price: <input type='text' defaultValue={''} {...register('price')}  /></label></div>
+    <div><label>Year: <input type='text' defaultValue={''} {...register('year')} /></label></div>
+    <button>Save</button>
 </form>
         </div>
     );
