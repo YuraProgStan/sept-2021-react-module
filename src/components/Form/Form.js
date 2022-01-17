a4a890496c14473953d7fa9b14e109675c1262a4
YuraProgStan <yurstan@gmail.com>
2022-01-17 22:11:17 +0200

    bonus_useForm_joi

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import  {useState} from 'react';
 import {useForm} from 'react-hook-form';
+import {carService} from '../../services/carService';
 
 const Form = () => {
+    const [formError, setFormError] = useState(null);
     const {
 register, handleSubmit, watch, formState:{errors}
 
     } = useForm();
-    const submit = (data) => {
-        carSevice.create(car).then(value => console.log(value)).catch(error => {
-            console.log(error.response);
+    const submit = (car) => {
+        carService.create(car).then(value => console.log(value)).catch(error => {
+            setFormError(error.response.data);
         })
     }
     // watch(event => console.log(event));
@@ -16,8 +19,11 @@ register, handleSubmit, watch, formState:{errors}
         <div>
 <form onSubmit = {handleSubmit(submit)}>
     <div><label>Model: <input type='text' defaultValue={''} {...register ('model')} /></label></div>
+    {formError.model  && <span>{formError.model.message}</span>}
     <div><label>Price: <input type='text' defaultValue={''} {...register('price')}  /></label></div>
+    {formError.price  && <span>{formError.price.message}</span>}
     <div><label>Year: <input type='text' defaultValue={''} {...register('year')} /></label></div>
+    {formError.year  && <span>{formError.year.message}</span>}
     <button>Save</button>
 </form>
         </div>
