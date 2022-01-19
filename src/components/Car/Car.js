1976949fd04cb8d5dae77c9d43cdd7bd8fde17f3
YuraProgStan <yurstan@gmail.com>
2022-01-19 19:48:36 +0200

    Bonus

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,13 +1,17 @@
-const Car = ({car, delete, getId}) => {
+import {carService} from "../../services/carService";
+
+const Car = ({car}) => {
     const {id, model, price, year} = car;
-    getId = id;
+    const delById =  (id) => {
+      carService.deleteById(id).then(res =>console.log(res.data));
+    }
     return (
         <div>
             <div>id: {id}</div>
             <div>model: {model}</div>
             <div>price: {price}</div>
             <div>year: {year}</div>
-            <button onClick={delete}>Delete</button>
+            <button onClick={delById}>Delete</button>
             <hr/>
         </div>
     );
--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -5,22 +5,15 @@ import Car from "../Car/Car";
 
 const Cars = ({trigger}) => {
     const [cars, setCars] = useState([]);
-    const id = getId;
-    const delById = async (id) => {
-        try {
-            await carService.deleteById(id);
 
-        }catch (error){
 
-        }
-    }
     useEffect(() => {
         carService.getAll().then(value => setCars([...value]))
     }, [trigger]);
 
     return (
         <div>
-            {cars.map(car => <Car key={car.id} car={car} delete={delById} getId = {getId} />)}
+            {cars.map(car => <Car key={car.id} car={car} />)}
         </div>
     );
 };
