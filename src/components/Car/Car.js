6edcdbc3885a83364b8aecd3ec7fda73362fe56b
YuraProgStan <yurstan@gmail.com>
2022-01-19 20:41:43 +0200

    Bonus. Допка delete car by ID и добавления нового car

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,17 +1,13 @@
-import {carService} from "../../services/carService";
 
-const Car = ({car}) => {
+const Car = ({car, delById}) => {
     const {id, model, price, year} = car;
-    const delById =  (id) => {
-      carService.deleteById(id).then(res =>console.log(res.data));
-    }
     return (
         <div>
             <div>id: {id}</div>
             <div>model: {model}</div>
             <div>price: {price}</div>
             <div>year: {year}</div>
-            <button onClick={delById}>Delete</button>
+            <button onClick={()=>delById(id)}>Delete</button>
             <hr/>
         </div>
     );
--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -5,15 +5,18 @@ import Car from "../Car/Car";
 
 const Cars = ({trigger}) => {
     const [cars, setCars] = useState([]);
+    const delById =  (id) => {
+        carService.deleteById(id);
+    }
 
 
     useEffect(() => {
         carService.getAll().then(value => setCars([...value]))
-    }, [trigger]);
+    }, [trigger, delById]);
 
     return (
         <div>
-            {cars.map(car => <Car key={car.id} car={car} />)}
+            {cars.map(car => <Car key={car.id} car={car} delById={delById} />)}
         </div>
     );
 };
