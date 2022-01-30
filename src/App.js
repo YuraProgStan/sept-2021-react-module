ac0f2a0df8dc9b6bbe50e05c033b8e643ce850fb
YuraProgStan <yurstan@gmail.com>
2022-01-30 12:02:16 +0200

    Local state cars, useForm, delById without Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {useState} from "react";
+import {Cars, Form} from "./components";
 
 const App = () => {
+const [cars, setCars] = useState([]);
+const getFormData = (data) => {
+setCars([...cars, {id: new Date().getTime(), ...data}])
+}
+const getCarId = (id) => {
+setCars(cars.filter(car =>car.id!==id))
+}
   return (
-    <div className="App">
-
-    </div>
+    <>
+<Form getFormData={getFormData} />
+<Cars cars = {cars} getCarId = {getCarId} />
+    </>
   );
 }
 
