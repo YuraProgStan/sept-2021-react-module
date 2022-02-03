a1af59f74619c890f86a45ed57a9db92446e4707
YuraProgStan <yurstan@gmail.com>
2022-02-03 20:24:14 +0200

    Homework TypeScript

--- main.ts
+++ main.ts
@@ -1,44 +1,123 @@
-/*
-2) протипизировать функции:
-
-    const user = {
-        name:"Max",
-        age:18,
-        gender:'male'
-    }
-
-function sum(a,b){
-    return a+b
-}
-function showSum(a,b){
-    console.log(a + b);
-}
-
-function incAge(someUser, inc){
-    someUser.age+=inc
-    return someUser
-}
-
-console.log(sum(1, 2));
-showSum(2,3)
-incAge(user, 2)*/
+import {IStarlink} from "./interfaces/starlink.interface";
 import {IUser} from "./interfaces/user.interface";
 
-const user:IUser = {
-    name:"Max",
-    age:18,
-    gender:'male'
+/*1) создать интерфейс на основе этого объекта:
+
+{
+    "mission_name": "Starlink-15 (v1.0)",
+    "launch_date_local": "2020-10-24T11:31:00-04:00",
+    "launch_site": {
+    "site_name_long": "Cape Canaveral Air Force Station Space Launch Complex 40"
+},
+    "links": {
+    "article_link": null,
+        "video_link": "https://youtu.be/J442-ti-Dhg"
+},
+    "rocket": {
+    "rocket_name": "Falcon 9",
+        "first_stage": {
+        "cores": [
+            {
+                "flight": 7,
+                "core": {
+                    "reuse_count": 6,
+                    "status": "unknown"
+                }
+            }
+        ]
+    },
+    "second_stage": {
+        "payloads": [
+            {
+                "payload_type": "Satellite",
+                "payload_mass_kg": 15400,
+                "payload_mass_lbs": 33951.2
+            }
+        ]
+    }
 }
-function sum(a:number,b:number):number{
-    return a+b
+}*/
+const starlink: IStarlink = {
+    "mission_name": "Starlink-15 (v1.0)",
+    "launch_date_local": "2020-10-24T11:31:00-04:00",
+    "launch_site": {
+        "site_name_long": "Cape Canaveral Air Force Station Space Launch Complex 40"
+    },
+    "links": {
+        "article_link": null,
+        "video_link": "https://youtu.be/J442-ti-Dhg"
+    },
+    "rocket": {
+        "rocket_name": "Falcon 9",
+        "first_stage": {
+            "cores": [
+                {
+                    "flight": 7,
+                    "core": {
+                        "reuse_count": 6,
+                        "status": "unknown"
+                    }
+                }
+            ]
+        },
+        "second_stage": {
+            "payloads": [
+                {
+                    "payload_type": "Satellite",
+                    "payload_mass_kg": 15400,
+                    "payload_mass_lbs": 33951.2
+                }
+            ]
+        }
+    }
 }
-function showSum(a:number,b:number):void{
+
+
+    /*
+    2) протипизировать функции:
+
+        const user = {
+            name:"Max",
+            age:18,
+            gender:'male'
+        }
+
+    function sum(a,b){
+        return a+b
+    }
+    function showSum(a,b){
+        console.log(a + b);
+    }
+
+    function incAge(someUser, inc){
+        someUser.age+=inc
+        return someUser
+    }
+
+    console.log(sum(1, 2));
+    showSum(2,3)
+    incAge(user, 2)*/
+
+
+const user: IUser = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
+
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
     console.log(a + b);
 }
-function incAge(someUser:IUser, inc:number):IUser{
-    someUser.age+=inc
+
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc
     return someUser
 }
+
 console.log(sum(1, 2));
-showSum(2,3);
+showSum(2, 3);
 incAge(user, 2);
