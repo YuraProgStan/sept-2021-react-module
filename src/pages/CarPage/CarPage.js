898e9dc900aea29cdebb8bf435663bf58592264a
YuraProgStan <yurstan@gmail.com>
2022-02-10 18:37:19 +0200

    loremflickr

--- src/pages/CarPage/CarPage.js
+++ src/pages/CarPage/CarPage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {UncachedImage} from "react-uncached-image";
 
 import {useConsume} from "../../hooks/consume";
 import baseUrl, {urls} from "../../configs/urls";
 import css from "../CatPage/CatPage.module.css";
+import axios from "axios";
 
 const CarPage = () => {
-    const {setImage} = useConsume();
+    const {image, setImage} = useConsume();
+    const [url3, setUrl] = useState(null);
+    const getImage3 = async () => {
+        const response = await fetch('https://loremflickr.com/800/600/car');
+        const imageBlob = await response.blob();
+        const reader = new FileReader();
+        reader.readAsDataURL(imageBlob);
+        reader.onloadend = () => {
+            const base64data = reader.result;
+            setUrl(base64data);
+        };
+    };
+
     const updateImage = () => {
-        setImage({})
+        axios.get('https://loremflickr.com/800/600/car')
+            .then(value => value.request.responseURL)
+            .then(responseURL => setImage(responseURL));
+        getImage3();
     }
+    useEffect(() => {
+        axios.get('https://loremflickr.com/800/600/car')
+            .then(value => value.request.responseURL)
+            .then(responseURL => setImage(responseURL))
+        updateImage();
+    }, [])
     let url = `${baseUrl}${urls.car}`;
 
     return (
@@ -17,6 +39,8 @@ const CarPage = () => {
             <h2>Car</h2>
             <div className={css.wrap}>
                 <div><UncachedImage src={url}/></div>
+                <div><img alt={'car'} src={image}/></div>
+                <div><img alt={'car'} src={url3}/></div>
                 <div>
                     <button onClick={() => updateImage()}>Update</button>
                 </div>
--- src/services/main.service.js
+++ src/services/main.service.js
@@ -1,5 +1,6 @@
 import {axiosService} from "./axios.service";
 
 export const mainService = {
-    getPhoto: (name) => axiosService.get(name).then(value => value.data)
+    getPhoto: (name) => axiosService.get(name).then(value=>value.request.responseURL)
+
 }
