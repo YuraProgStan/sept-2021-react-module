278d9dacf3e92b70707c8456be9764c34d1b8b2f
YuraProgStan <yurstan@gmail.com>
2022-02-13 20:03:02 +0200

    React Redux (not toolkit)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/home-page/HomePage";
+import {UsersPage} from "./pages/users-page/UsersPage";
+import {CommentsPage} from "./pages/comments-page/CommentsPage";
+import {PostsPage} from "./pages/posts-page/PostsPage";
 
-const App = () => {
+function App() {
+
+
+
   return (
-    <div className="App">
+      <div>
+        <ul>
+          <li><Link to={'/'}>home page</Link></li>
+          <li><Link to={'/users'}>users page</Link></li>
+          <li><Link to={'/posts'}>posts page</Link></li>
+          <li><Link to={'/comments'}>comments page</Link></li>
+        </ul>
 
-    </div>
+        <Routes>
+          <Route path={'/'} element={<HomePage/>}/>
+          <Route path={'/users'} element={<UsersPage/>}/>
+          <Route path={'/posts'} element={<PostsPage/>}/>
+          <Route path={'/comments'} element={<CommentsPage/>}/>
+        </Routes>
+
+      </div>
   );
 }
 
---
+++ src/pages/users-page/UsersPage.js
@@ -0,0 +1,21 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import {fetchUsers} from '../../redux/users.actionCreator';
+
+export function UsersPage() {
+
+    let {users, isUsersLoading} = useSelector(state => state.usersReducer);
+
+    let dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+    return (
+        <div>
+            {
+                isUsersLoading && <h3 style={{color: 'red'}}>Loading ...</h3>
+            }
+
+        </div>
+    );
+}
